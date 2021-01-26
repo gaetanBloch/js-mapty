8ef3dbb6f606d8ed7cecf8721293909c669027aa
gbloch <gaetan.bloch@gmail.com>
2021-01-26 04:18:35 +0100

    Get the geolocation coordinates from the navigator API

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-'use strict';
-
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -10,3 +20,23 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+const buildMapUri = (
+  latitude,
+  longitude,
+) => `https://www.google.fr/maps/@${latitude},${longitude}`;
+
+const init = () => {
+  const location = navigator.geolocation;
+  if (!location) return;
+  location.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      console.log(buildMapUri(latitude, longitude));
+    }, (error) => {
+      console.log(error);
+    },
+  );
+};
+
+init();
