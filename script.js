8773851bffd7d4009fd1216f5c9314347d614f73
gbloch <gaetan.bloch@gmail.com>
2021-01-26 07:14:44 +0100

    Refactoring

--- script.js
+++ script.js
@@ -23,35 +23,39 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+const click = function handleMapClick(mapEvent) {
+  L.marker([mapEvent.latlng.lat, mapEvent.latlng.lng])
+    .addTo(this)
+    .bindPopup(L.popup({
+      maxWidth: 200,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+      className: 'running-popup',
+    }).setContent('Running'))
+    .openPopup();
+};
+
+const displayMap = (position) => {
+  const map = L.map('map')
+    .setView([
+      position.coords.latitude,
+      position.coords.longitude,
+    ], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  map.on('click', click.bind(map));
+};
+
 const init = () => {
   const location = navigator.geolocation;
   if (!location) return;
   location.getCurrentPosition(
-    (position) => {
-      const map = L.map('map')
-        .setView([
-          position.coords.latitude,
-          position.coords.longitude,
-        ], 13);
-
-      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      map.on('click', (mapEvent) => {
-        L.marker([mapEvent.latlng.lat, mapEvent.latlng.lng])
-          .addTo(map)
-          .bindPopup(L.popup({
-            maxWidth: 200,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-            .setContent('Running'))
-          .openPopup();
-      });
-    }, (error) => {
+    displayMap,
+    (error) => {
       throw new Error(error.message);
     },
   );
