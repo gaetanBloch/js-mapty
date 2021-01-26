2c241d593a694438f05b8a91d63bc04072941575
gbloch <gaetan.bloch@gmail.com>
2021-01-26 05:41:18 +0100

    Change map style

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const init = () => {
       const coords = [latitude, longitude];
 
       const map = L.map('map').setView(coords, 13);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
       L.marker(coords).addTo(map)
