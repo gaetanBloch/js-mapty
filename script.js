4ad55633a1ac8596d50a4a39a4de11ba4ff05f80
gbloch <gaetan.bloch@gmail.com>
2021-01-26 07:35:17 +0100

    Display the marker on workout form submission

--- script.js
+++ script.js
@@ -23,11 +23,36 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-const click = function handleMapClick(mapEvent) {
+let map;
+let mapEvent;
+
+const handleMapClick = (event) => {
+  mapEvent = event;
   form.classList.remove('hidden');
+  inputDistance.focus();
+};
+
+const displayMap = (position) => {
+  map = L.map('map')
+    .setView([
+      position.coords.latitude,
+      position.coords.longitude,
+    ], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  map.on('click', handleMapClick);
+};
+
+const displayMarker = (event) => {
+  event.preventDefault();
+
+  form.classList.add('hidden');
 
   L.marker([mapEvent.latlng.lat, mapEvent.latlng.lng])
-    .addTo(this)
+    .addTo(map)
     .bindPopup(L.popup({
       maxWidth: 200,
       minWidth: 100,
@@ -38,22 +63,9 @@ const click = function handleMapClick(mapEvent) {
     .openPopup();
 };
 
-const displayMap = (position) => {
-  const map = L.map('map')
-    .setView([
-      position.coords.latitude,
-      position.coords.longitude,
-    ], 13);
-
-  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
-
-  map.on('click', click.bind(map));
-};
-
 const init = () => {
   form.classList.add('hidden');
+  form.addEventListener('submit', displayMarker);
 
   const location = navigator.geolocation;
   if (!location) return;
